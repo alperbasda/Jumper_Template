584457b6708432a13ba7f49b13f8928d947faacf
alperbasd <125950998+alperbasd@users.noreply.github.com>
2023-10-21 17:21:41 +0300

    Api template yenilendi.

---
+++ CqrsTemplatePack.Creator/content/Presentation/CqrsTemplatePack.Web.Api/Controllers/Base/MediatrController.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CqrsTemplatePack.UI.Api
+{
+    public class MediatrController : Controller
+    {
+        private IMediator? _mediator;
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+    }
+}
